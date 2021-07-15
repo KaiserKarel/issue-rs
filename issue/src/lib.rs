b3bf968a4c36b0fa84f3a14a84d3b09aeec65f23
kaiserkarel <k.l.kubat@gmail.com>
2021-07-16 00:35:22 +0200

    cleanup

--- issue/src/lib.rs
+++ issue/src/lib.rs
@@ -1,8 +1,6 @@
-#![warn(
-    clippy::all,
-    clippy::restriction,
-    clippy::pedantic,
-    clippy::nursery,
-    clippy::cargo
-)]
+#![warn(clippy::all, clippy::pedantic, clippy::nursery, missing_docs)]
+
+//!
+//!
+
 pub use ::macros::track;
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -4,7 +4,49 @@ use proc_macro::TokenStream;
 use proc_macro_error::{emit_error, emit_warning, proc_macro_error};
 use syn::{parse_macro_input, spanned::Spanned, AttributeArgs};
 
-static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
+const APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
+
+/// Fetches the issue from Github and emits a warning or error if it is closed depending on the
+/// `ISSUE_HARD_FAIL` environment variable.
+#[proc_macro_error]
+#[proc_macro_attribute]
+pub fn track(args: TokenStream, input: TokenStream) -> TokenStream {
+    let attr_args = parse_macro_input!(args as AttributeArgs);
+
+    let issue = match Issue::from_list(&attr_args) {
+        Ok(v) => v,
+        Err(e) => {
+            return TokenStream::from(e.write_errors());
+        }
+    };
+
+    match issue.is_closed() {
+        Err(e) => {
+            emit_error!(
+                attr_args[0].span(),
+                "unable to access {}\n  {}",
+                issue.url,
+                e
+            )
+        }
+        Ok(true) => {
+            if std::env::var("ISSUE_HARD_FAIL").is_ok() {
+                emit_error!(attr_args[0].span(), "issue {} has been closed", issue.url)
+            } else {
+                emit_warning!(attr_args[0].span(), "issue {} has been closed", issue.url)
+            }
+        }
+        _ => (),
+    }
+
+    input
+}
+
+// Both Github and Gitlab use the `closed_at` field to identify closed issues.
+#[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+struct ApiIssue {
+    pub closed_at: Option<String>,
+}
 
 #[derive(Default, FromMeta)]
 #[darling(default)]
@@ -45,44 +87,4 @@ impl Issue {
 
         Ok(issue.closed_at.is_some())
     }
-}
-
-#[proc_macro_error]
-#[proc_macro_attribute]
-pub fn track(args: TokenStream, input: TokenStream) -> TokenStream {
-    let attr_args = parse_macro_input!(args as AttributeArgs);
-
-    let issue = match Issue::from_list(&attr_args) {
-        Ok(v) => v,
-        Err(e) => {
-            return TokenStream::from(e.write_errors());
-        }
-    };
-
-    match issue.is_closed() {
-        Err(e) => {
-            emit_error!(
-                attr_args[0].span(),
-                "unable to access {}\n  {}",
-                issue.url,
-                e
-            )
-        }
-        Ok(true) => {
-            if std::env::var("ISSUE_HARD_FAIL").is_ok() {
-                emit_error!(attr_args[0].span(), "issue {} has been closed", issue.url)
-            } else {
-                emit_warning!(attr_args[0].span(), "issue {} has been closed", issue.url)
-            }
-        }
-        _ => (),
-    }
-
-    input
-}
-
-// Both Github and Gitlab use the `closed_at` field to identify closed issues.
-#[derive(Default, Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
-struct ApiIssue {
-    pub closed_at: Option<String>,
 }

e25fb76d0babe9773f2044f8abce8d88a05b8e03
kaiserkarel <k.l.kubat@gmail.com>
2021-07-28 17:24:33 +0200

    exit with status 1 on open issues

--- cargo-issue/src/main.rs
+++ cargo-issue/src/main.rs
@@ -2,6 +2,8 @@ use futures::stream::StreamExt;
 use issue::{GithubIssue, APP_USER_AGENT, CONFIG_ENV};
 use serde_json::Deserializer;
 use std::process::{Command, Stdio};
+use std::sync::atomic::{AtomicI32, Ordering};
+use std::sync::Arc;
 use structopt::StructOpt;
 use tokio::time::Duration;
 use tokio_stream::{self as stream};
@@ -47,7 +49,7 @@ pub enum Cmd {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), anyhow::Error> {
     let Cli::Issue(opts) = Cli::from_args();
 
     let (package, manifest_path) = match opts.clone() {
@@ -87,22 +89,25 @@ async fn main() {
     // Ensures that cargo's build output is not interleaved with the actual issues being printed.
     tokio::time::sleep(Duration::from_millis(100)).await;
 
+    let exit_code = Arc::new(AtomicI32::new(0));
+
     stream::iter(stream)
         .for_each_concurrent(None, |issue| {
             let opts = opts.clone();
-
+            let exit_code = exit_code.clone();
             async move {
                 let issue = issue.expect("deserializing cargo check stdout");
-
                 match opts {
                     Cmd::List { .. } => println!("{}", &issue),
                     Cmd::Check { .. } => {
                         #[allow(clippy::blocks_in_if_conditions)]
                         if is_closed(&issue).await.unwrap_or_else(|e| {
                             eprintln!("unable to access issue: {}", e);
+                            exit_code.store(1, Ordering::SeqCst);
                             false
                         }) {
-                            println!("Closed issue: \n\n       - url: {}", &issue.url)
+                            eprintln!("Closed issue: {}", &issue.url);
+                            exit_code.store(1, Ordering::SeqCst);
                         }
                     }
                 }
@@ -111,6 +116,11 @@ async fn main() {
         .await;
 
     handle.wait().unwrap();
+
+    if exit_code.load(Ordering::SeqCst) == 1 {
+        anyhow::bail!("encountered errors")
+    }
+    Ok(())
 }
 
 pub async fn is_closed(issue: &issue::Issue) -> Result<bool, anyhow::Error> {
